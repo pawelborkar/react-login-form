00feeddd806b0422b866071e2ce32f305825dd44
Pawel Borkar <pawelborkar@aol.com>
2022-01-18 23:48:57 +0530

    Add FormValues and isSubmit to dependencies array of useEffect hook

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
         if (Object.keys(formErrors).length === 0 && isSubmit) {
             console.log(formValues);
         }
-    }, [formErrors]);
+    }, [formErrors, formValues, isSubmit]);
     const validate = (values) => {
         const errors = {};
         const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
