609efd825e51387317954d5a0c9209404f51fc99
Pawel Borkar <pawelborkar@aol.com>
2022-01-18 23:34:43 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,101 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const initialValues = { username: "", email: "", password: "" };
+    const [formValues, setFormValues] = useState(initialValues);
+    const [formErrors, setFormErrors] = useState({});
+    const [isSubmit, setIsSubmit] = useState(false);
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormValues({ ...formValues, [name]: value });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setFormErrors(validate(formValues));
+        setIsSubmit(true);
+    };
+
+    useEffect(() => {
+        console.log(formErrors);
+        if (Object.keys(formErrors).length === 0 && isSubmit) {
+            console.log(formValues);
+        }
+    }, [formErrors]);
+
+    const validate = (values) => {
+        const errors = {};
+        const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+        if (!values.username) {
+            errors.username = "Username is required!";
+        }
+        if (!values.email) {
+            errors.email = "Email is required!";
+        } else if (!regex.test(values.email)) {
+            errors.email = "Please enter a valid email address";
+        }
+        if (!values.password) {
+            errors.password = "Password is required";
+        } else if (values.password.length < 6) {
+            errors.password = "Password length should be atleast 6 characters";
+        } else if (values.password.length > 16) {
+            errors.password =
+                "Password lenght should not be greater than 16 characters";
+        }
+        return errors;
+    };
+    return (
+        <div className="container">
+            {Object.keys(formErrors).length === 0 && isSubmit ? (
+                <div className="ui message success">Signed in successfully</div>
+            ) : (
+                <pre>{JSON.stringify(formValues, undefined, 2)}</pre>
+            )}
+
+            <form onSubmit={handleSubmit}>
+                <h1>Login form</h1>
+                <div className="ui divider"></div>
+                <div className="ui form">
+                    <div className="field">
+                        <label>Username</label>
+                        <input
+                            type="text"
+                            name="username"
+                            placeholder="Username"
+                            value={formValues.username}
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <p>{formErrors.username}</p>
+                    <div className="field">
+                        <label>Email</label>
+                        <input
+                            typle="text"
+                            name="email"
+                            placeholder="abc@domain.com"
+                            value={formValues.email}
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <p>{formErrors.email}</p>
+                    <div className="field">
+                        <label>Password</label>
+                        <input
+                            type="password"
+                            name="password"
+                            placeholder="Password"
+                            value={formValues.password}
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <p>{formErrors.password}</p>
+                    <button className="fluid ui button blue">Submit</button>
+                </div>
+            </form>
+        </div>
+    );
 }
 
 export default App;
