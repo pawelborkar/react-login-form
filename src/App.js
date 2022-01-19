538225e1f01844e1b6a4857f2fa0d5f0c24bfbbd
Pawel Borkar <pawelborkar@aol.com>
2022-01-19 17:58:38 +0530

    Version 0.2

--- src/App.js
+++ src/App.js
@@ -2,7 +2,12 @@ import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-    const initialValues = { username: "", email: "", password: "" };
+    const initialValues = {
+        username: "",
+        email: "",
+        password: "",
+        confirmPassword: "",
+    };
     const [formValues, setFormValues] = useState(initialValues);
     const [formErrors, setFormErrors] = useState({});
     const [isSubmit, setIsSubmit] = useState(false);
@@ -42,58 +47,80 @@ function App() {
         } else if (values.password.length > 10) {
             errors.password = "Password cannot exceed more than 10 characters";
         }
+        if (values.password != values.confirmPassword) {
+            errors.confirmPassword = "Those passwords didn’t match. Try again.";
+        }
         return errors;
     };
 
     return (
-        <div className="container">
-            {Object.keys(formErrors).length === 0 && isSubmit ? (
-                <div className="ui message success">Signed in successfully</div>
-            ) : (
-                <pre>{JSON.stringify(formValues, undefined, 2)}</pre>
-            )}
+        <>
+            <div className="bgImg"></div>
+            <div className="container">
+                {Object.keys(formErrors).length === 0 && isSubmit ? (
+                    <div className="ui message success">
+                        Signed in successfully
+                    </div>
+                ) : (
+                    console.log("Entered Details", formValues)
+                )}
 
-            <form onSubmit={handleSubmit}>
-                <h1>Login Form</h1>
-                <div className="ui divider"></div>
-                <div className="ui form">
-                    <div className="field">
-                        <label>Username</label>
-                        <input
-                            type="text"
-                            name="username"
-                            placeholder="Username"
-                            value={formValues.username}
-                            onChange={handleChange}
-                        />
+                <form onSubmit={handleSubmit}>
+                    <h1>Sign Up</h1>
+                    <div className="ui divider"></div>
+                    <div className="ui form">
+                        <div className="field">
+                            <label>Username</label>
+                            <input
+                                type="text"
+                                name="username"
+                                placeholder="Choose a username"
+                                value={formValues.username}
+                                onChange={handleChange}
+                            />
+                        </div>
+                        <p>{formErrors.username}</p>
+                        <div className="field">
+                            <label>Email</label>
+                            <input
+                                type="text"
+                                name="email"
+                                placeholder="Email"
+                                value={formValues.email}
+                                onChange={handleChange}
+                            />
+                        </div>
+                        <p>{formErrors.email}</p>
+                        <div className="field">
+                            <label>Password</label>
+                            <input
+                                type="password"
+                                name="password"
+                                placeholder="Password"
+                                value={formValues.password}
+                                onChange={handleChange}
+                            />
+                        </div>
+                        <p>{formErrors.password}</p>
+                        <div className="field">
+                            <label>Confirm Password</label>
+                            <input
+                                type="password"
+                                name="confirmPassword"
+                                placeholder="Confirm password"
+                                value={formValues.confirmPassword}
+                                onChange={handleChange}
+                            />
+                        </div>
+                        <p>{formErrors.confirmPassword}</p>
+                        <button className="fluid ui button blue">Submit</button>
                     </div>
-                    <p>{formErrors.username}</p>
-                    <div className="field">
-                        <label>Email</label>
-                        <input
-                            type="text"
-                            name="email"
-                            placeholder="Email"
-                            value={formValues.email}
-                            onChange={handleChange}
-                        />
-                    </div>
-                    <p>{formErrors.email}</p>
-                    <div className="field">
-                        <label>Password</label>
-                        <input
-                            type="password"
-                            name="password"
-                            placeholder="Password"
-                            value={formValues.password}
-                            onChange={handleChange}
-                        />
-                    </div>
-                    <p>{formErrors.password}</p>
-                    <button className="fluid ui button blue">Submit</button>
+                </form>
+                <div className="text">
+                    Already have an account? <span>Login</span>
                 </div>
-            </form>
-        </div>
+            </div>{" "}
+        </>
     );
 }
 
